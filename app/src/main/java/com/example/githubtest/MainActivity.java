71d5120ac1ee883b17e14b8359d9acffba1c5e9c
sww <jadore12000@gmail.com>
2022-10-06 23:27:20 +0900

    ㅇㅇㅇㅇ

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //수정
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
